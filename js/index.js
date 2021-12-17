1e8a0f94a280e0ec3176cde54c6476abf00c4cf3
MuxtorbekDev <muxtorbekravshanov9@gmail.com>
2021-12-17 15:44:47 +0500

    main section

--- js/index.js
+++ js/index.js
@@ -40,3 +40,20 @@ navMenuIcon.addEventListener("click", () => {
   navMenuIcon.classList.toggle("bx-x");
 });
 AOS.init();
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 5500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
