0e0e91b4aa48393ff0453f241a72210681c80bd6
MuxtorbekDev <muxtorbekravshanov9@gmail.com>
2021-12-18 00:45:32 +0500

    featured section qilindi

--- js/index.js
+++ js/index.js
@@ -57,3 +57,33 @@ var swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+function hoverBottomAdd(hover, bottom, text) {
+  const hoverBlock = document.querySelector(hover);
+  const bottomAdd = document.querySelector(bottom);
+  const textSection = document.querySelector(text);
+
+  hoverBlock.addEventListener("mousemove", () => {
+    bottomAdd.classList.add("bottom3");
+    textSection.classList.add("bottom2");
+  });
+
+  hoverBlock.addEventListener("mouseout", () => {
+    bottomAdd.classList.remove("bottom3");
+    textSection.classList.remove("bottom2");
+  });
+}
+
+hoverBottomAdd(".featured .game", ".game .text button", ".game .text");
+hoverBottomAdd(".featured .keybo", ".keybo .text button", ".keybo .text");
+hoverBottomAdd(".featured .speaker", ".speaker .text button", ".speaker .text");
+hoverBottomAdd(
+  ".featured .headphone",
+  ".headphone .text button",
+  ".headphone .text"
+);
+hoverBottomAdd(".featured .ultra", ".ultra .text button", ".ultra .text");
+hoverBottomAdd(".featured .gadg", ".gadg .text button", ".gadg .text");
+hoverBottomAdd(".featured .graph", ".graph .text button", ".graph .text");
+hoverBottomAdd(".featured .bluet", ".bluet .text button", ".bluet .text");
+hoverBottomAdd(".featured .habby", ".habby .text button", ".habby .text");
